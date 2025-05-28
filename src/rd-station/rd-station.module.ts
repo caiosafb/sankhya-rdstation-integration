ffbcdfc093f7db1a3c713df4a38f78746f079569
Caio <caiobarcellos16@gmail.com>
2025-05-28 15:17:09 -0300

    feat(rd-station): add rd station module structure

---
+++ src/rd-station/rd-station.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import { BullModule } from '@nestjs/bull';
+import { RdStationService } from './rd-station.service';
+import { RdStationController } from './rd-station.controller';
+
+@Module({
+  imports: [
+    HttpModule,
+    BullModule.registerQueue({
+      name: 'rd-station-queue',
+    }),
+  ],
+  controllers: [RdStationController],
+  providers: [RdStationService],
+  exports: [RdStationService],
+})
+export class RdStationModule {}
