481006741221e458c68442fd5ed1ceeecefd14e9
Caio <caiobarcellos16@gmail.com>
2025-05-28 15:17:09 -0300

    feat(sankhya): add sankhya module structure

---
+++ src/sankhya/sankhya.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import { BullModule } from '@nestjs/bull';
+import { SankhyaService } from './sankhya.service';
+import { SankhyaController } from './sankhya.controller';
+
+@Module({
+  imports: [
+    HttpModule,
+    BullModule.registerQueue({
+      name: 'sankhya-queue',
+    }),
+  ],
+  controllers: [SankhyaController],
+  providers: [SankhyaService],
+  exports: [SankhyaService],
+})
+export class SankhyaModule {}
