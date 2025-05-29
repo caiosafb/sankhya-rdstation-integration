b6332a7d444716a740bcb1143ae02f897b61083b
Caio <caiobarcellos16@gmail.com>
2025-05-29 16:48:55 -0300

    feat(sankhya): configure module with Bull queue support

--- src/sankhya/sankhya.module.ts
+++ src/sankhya/sankhya.module.ts
@@ -1,14 +1,14 @@
-import { Module } from '@nestjs/common';
-import { HttpModule } from '@nestjs/axios';
-import { BullModule } from '@nestjs/bull';
-import { SankhyaService } from './sankhya.service';
-import { SankhyaController } from './sankhya.controller';
+import { Module } from "@nestjs/common";
+import { HttpModule } from "@nestjs/axios";
+import { BullModule } from "@nestjs/bull";
+import { SankhyaService } from "./sankhya.service";
+import { SankhyaController } from "./sankhya.controller";
 
 @Module({
   imports: [
     HttpModule,
     BullModule.registerQueue({
-      name: 'sankhya-queue',
+      name: "sankhya-queue",
     }),
   ],
   controllers: [SankhyaController],
