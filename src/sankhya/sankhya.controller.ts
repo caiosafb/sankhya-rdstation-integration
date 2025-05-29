ea3a8f1897b5c7e672e7a7e4017d2f575c17f9fe
Caio <caiobarcellos16@gmail.com>
2025-05-29 16:48:27 -0300

    feat(sankhya): update controller with complete endpoints

--- src/sankhya/sankhya.controller.ts
+++ src/sankhya/sankhya.controller.ts
@@ -1,42 +1,42 @@
-import { Controller, Get, Post, Body, Query } from '@nestjs/common';
-import { SankhyaService } from './sankhya.service';
-import { CreateFornecedorDto, CreatePedidoDto } from './dto';
+import { Controller, Get, Post, Body, Query } from "@nestjs/common";
+import { SankhyaService } from "./sankhya.service";
+import { CreateFornecedorDto, CreatePedidoDto } from "./dto";
 
-@Controller('sankhya')
+@Controller("sankhya")
 export class SankhyaController {
   constructor(private readonly sankhyaService: SankhyaService) {}
 
-  @Get('fornecedores')
+  @Get("fornecedores")
   async getFornecedores(@Query() filters: any) {
     return this.sankhyaService.getFornecedores(filters);
   }
 
-  @Post('fornecedores')
+  @Post("fornecedores")
   async createFornecedor(@Body() fornecedor: CreateFornecedorDto) {
     return this.sankhyaService.createFornecedor(fornecedor);
   }
 
-  @Get('empresas')
+  @Get("empresas")
   async getEmpresas(@Query() filters: any) {
     return this.sankhyaService.getEmpresas(filters);
   }
 
-  @Get('produtos')
+  @Get("produtos")
   async getProdutos(@Query() filters: any) {
     return this.sankhyaService.getProdutos(filters);
   }
 
-  @Get('pedidos')
+  @Get("pedidos")
   async getPedidos(@Query() filters: any) {
     return this.sankhyaService.getPedidos(filters);
   }
 
-  @Post('pedidos')
+  @Post("pedidos")
   async createPedido(@Body() pedido: CreatePedidoDto) {
     return this.sankhyaService.createPedido(pedido);
   }
 
-  @Get('vendedores')
+  @Get("vendedores")
   async getVendedores(@Query() filters: any) {
     return this.sankhyaService.getVendedores(filters);
   }
